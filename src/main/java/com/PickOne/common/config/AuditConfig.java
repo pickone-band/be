51f2acb712ea0e8fc66b1243e7c81775e7fc917b
haechangchoi <buraldongja@naver.com>
2025-04-12 23:11:59 +0900

    ♻️ [refactor]: 공통 엔티티 및 jpaAuditing 수정
    - 엔티티 생명주기 주석처리, 추후 개선

--- src/main/java/com/PickOne/common/config/AuditConfig.java
+++ src/main/java/com/PickOne/common/config/AuditConfig.java
@@ -2,6 +2,7 @@ package com.PickOne.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -10,13 +11,16 @@ import java.util.Optional;
 @Configuration
 @EnableJpaAuditing
 public class AuditConfig {
-    // 현재 사용자 정보를 제공하는 Bean
+
     @Bean
-    public AuditorAware<String> auditorProvider() {
-        return () -> {
-            // 실제 구현에서는 SecurityContextHolder 등을 통해
-            // 현재 인증된 사용자 정보를 반환
-            return Optional.of("SYSTEM"); // 임시 하드코딩
-        };
+    @Profile("test")
+    public AuditorAware<String> testAuditorProvider() {
+        return () -> Optional.of("TEST_USER");
+    }
+
+    @Bean
+    @Profile("!test")
+    public AuditorAware<String> productionAuditorProvider() {
+        return () -> Optional.of("SYSTEM_USER");
     }
 }
