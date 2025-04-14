9a2ed6df59084c44ed4d9952fe6e62b39b69561d
haechangchoi <buraldongja@naver.com>
2025-04-14 23:26:29 +0900

    ✨ [feat]: User DAO 계층
    - 기본적인 저장, 조회, 삭제
    - 기본적인 Spring Data Jpa 구현체

---
+++ src/main/java/com/PickOne/user/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.PickOne.user.repository;
+
+import com.PickOne.user.model.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository {
+
+    User save(User user);
+    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
+    List<User> findAll();
+    void delete(User user);
+
+}
