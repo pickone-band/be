a38fbd4c7946972840bdb6dc0963f69046f97b1b
haechangchoi <buraldongja@naver.com>
2025-04-15 16:06:00 +0900

    ✨ [feat]: 유저 관련 에러 및 핸들러

---
+++ src/main/java/com/PickOne/user/exception/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package com.PickOne.user.exception;
+
+public class InvalidPasswordException extends UserException {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
