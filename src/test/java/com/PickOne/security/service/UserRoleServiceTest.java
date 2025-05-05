c0909f08a0da8765e19bde3d77ad900ef31d3635
haechangchoi <buraldongja@naver.com>
2025-05-05 16:15:36 +0900

    🚀 [deploy]: 젠킨스 충돌 해결
    - @MockitoSettings(strictness = Strictness.LENIENT)
    - 상태를 변경하는 테스트(SecurityContextHolder 등)는 테스트 후 정리 로직 추가

--- src/test/java/com/PickOne/security/service/UserRoleServiceTest.java
+++ src/test/java/com/PickOne/security/service/UserRoleServiceTest.java
@@ -14,10 +14,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
 
@@ -27,6 +28,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class UserRoleServiceTest {
 
     @Mock
