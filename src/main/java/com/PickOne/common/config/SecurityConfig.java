0e20edad627a7fde22e7c6d06a95e771db3c74c9
haechangchoi <buraldongja@naver.com>
2025-05-05 17:38:32 +0900

    🚀 [deploy]: 젠킨스 충돌 해결
    - cors 전부 허용

--- src/main/java/com/PickOne/common/config/SecurityConfig.java
+++ src/main/java/com/PickOne/common/config/SecurityConfig.java
@@ -77,10 +77,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://yourfrontend.com"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("*")); // 모든 오리진 허용
+        configuration.setAllowedMethods(List.of("*")); // 모든 메서드 허용
+        configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
+        configuration.setAllowCredentials(false); // 필요에 따라 조정 가능
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
