612504282db03b668024d3c874ef75003332caa5
haechangchoi <buraldongja@naver.com>
2025-05-06 19:14:55 +0900

    ♻️ [refactor]: test 코드 수정
    - 필요없는 설정 제거
    - @Import(NoSecurityConfig.class)
    @SpringBootTest(properties = "spring.main.allow-bean-definition-overriding=true") 제거

--- src/test/java/com/PickOne/domain/recruitments/service/RecruitmentServiceTest.java
+++ src/test/java/com/PickOne/domain/recruitments/service/RecruitmentServiceTest.java
@@ -23,8 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(NoSecurityConfig.class)
-@SpringBootTest(properties = "spring.main.allow-bean-definition-overriding=true")
+@SpringBootTest
 @Transactional
 class RecruitmentServiceTest {
 
