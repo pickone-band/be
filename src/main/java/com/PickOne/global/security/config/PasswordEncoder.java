6e3d7abfbceac40292f2eff26ab9081c080faf5c
buraldongja <buraldongja@naver.com>
2025-06-10 15:16:15 +0900

    🐛 [fix]: 순환 참조 해결

--- src/main/java/com/PickOne/global/security/config/PasswordEncoder.java
+++
@@ -1,6 +0,0 @@
-package com.PickOne.global.security.config;
-
-public interface PasswordEncoder {
-    String encode(String rawPassword);
-    boolean matches(String rawPassword, String encodedPassword);
-}
