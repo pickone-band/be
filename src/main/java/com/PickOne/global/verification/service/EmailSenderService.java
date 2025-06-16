6fefe7c5c3792b2feca8894c84bb58f9e726fd5e
buraldongja <buraldongja@naver.com>
2025-06-16 16:47:04 +0900

    ♻️ [refactor]: 서비스 리팩토링

--- src/main/java/com/PickOne/global/verification/service/EmailSenderService.java
+++ src/main/java/com/PickOne/global/verification/service/EmailSenderService.java
@@ -1,6 +1,6 @@
 package com.PickOne.global.verification.service;
 
-import com.PickOne.global.verification.model.domain.EmailMessage;
+import com.PickOne.global.verification.dto.EmailMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -25,23 +25,23 @@ public class EmailSenderService {
 
     public void sendEmail(EmailMessage emailMessage) {
         if (!emailEnabled) {
-            log.info("이메일 발송이 비활성화되어 있습니다. 다음 이메일이 발송되었을 것입니다: {}", emailMessage.getTo());
+            log.info("이메일 발송이 비활성화되어 있습니다. 다음 이메일이 발송되었을 것입니다: {}", emailMessage.to());
             return;
         }
 
         try {
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(emailMessage.getTo());
-            message.setSubject(emailMessage.getSubject());
-            message.setText(emailMessage.getBody());
+            message.setTo(emailMessage.to());
+            message.setSubject(emailMessage.subject());
+            message.setText(emailMessage.body());
             message.setFrom(fromEmail);
 
             mailSender.send(message);
 
-            log.info("{}로 이메일이 성공적으로 발송되었습니다.", emailMessage.getTo());
+            log.info("{}로 이메일이 성공적으로 발송되었습니다.", emailMessage.to());
 
         } catch (Exception e) {
-            log.error("{}로 이메일 발송에 실패했습니다: {}", emailMessage.getTo(), e.getMessage(), e);
+            log.error("{}로 이메일 발송에 실패했습니다: {}", emailMessage.to(), e.getMessage(), e);
             throw new EmailSendException("이메일 발송 중 오류가 발생했습니다: " + e.getMessage(), e);
         }
     }
