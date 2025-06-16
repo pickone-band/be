818982996cada7ee25da26f93bc1d4eb75d0508c
buraldongja <buraldongja@naver.com>
2025-06-16 21:37:20 +0900

    ♻️ [refactor]:이벤트 추가

---
+++ src/main/java/com/PickOne/domain/notification/model/domain/ApplicationResultEvent.java
@@ -0,0 +1,27 @@
+package com.PickOne.domain.notification.model.domain;
+
+public class ApplicationResultEvent extends BaseUserEvent {
+
+    private final Long recipientId; // 지원자 ID
+    private final String recruitmentTitle;
+    private final boolean accepted; // true: 수락, false: 거절
+
+    public ApplicationResultEvent(Long senderId, Long recipientId, String recruitmentTitle, boolean accepted) {
+        super(senderId); // 보낸 사람 = 모집자
+        this.recipientId = recipientId;
+        this.recruitmentTitle = recruitmentTitle;
+        this.accepted = accepted;
+    }
+
+    public Long getRecipientId() {
+        return recipientId;
+    }
+
+    public String getRecruitmentTitle() {
+        return recruitmentTitle;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+}
