480cb2ff19d960daa6cbf1152c5c8469fba0ae8e
woong <153367605+kendrickhama@users.noreply.github.com>
2025-07-09 11:37:47 +0900

    ✨ [feat]: S3 버킷 파일 업로드

---
+++ src/main/java/com/pickone/global/s3/TestFileEntity.java
@@ -0,0 +1,22 @@
+package com.pickone.global.s3;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class TestFileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imgText;
+    @Column(length=2048)
+    private String imgUrl;
+
+}
