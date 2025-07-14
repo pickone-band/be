8e45fbce0c065cd54ce45fa81538ab1a1bbbb5a3
buraldongja <buraldongja@naver.com>
2025-07-14 19:49:42 +0900

    ♻️ [refactor]: 웹소켓 configureMessageConverters 추가

--- src/main/java/com/pickone/global/websocket/config/WebSocketConfig.java
+++ src/main/java/com/pickone/global/websocket/config/WebSocketConfig.java
@@ -1,9 +1,14 @@
 package com.pickone.global.websocket.config;
 
 import com.pickone.global.websocket.intercepter.WebSocketAuthIntercepter;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -34,5 +39,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureClientInboundChannel(ChannelRegistration registration) {
     registration.interceptors(webSocketAuthIntercepter);
+  }
+
+  @Override
+  public boolean configureMessageConverters(List<MessageConverter> converters) {
+    DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+    resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+
+    MappingJackson2MessageConverter converter =
+        new MappingJackson2MessageConverter();
+    converter.setContentTypeResolver(resolver);
+
+    converters.add(converter);
+    return false;  // 기본 컨버터 대신 이 JSON 컨버터만 사용
   }
 }
